083bc011f4fadbac450502a149be8b8857c2bd1c
kendraregmi <kendra.regmi@gmail.com>
2018-11-30 11:45:48 +0545

    v1

---
+++ into.py
@@ -0,0 +1,12 @@
+import sys
+import numpy as n
+import cv2
+
+img=cv2.imread('sample.jpg',0)
+img = cv2.resize(img, (640, 640))
+print(img)
+cv2.imshow('img',img)
+neg=255-img
+print(neg)
+cv2.imshow('neg',neg)
+cv2.waitKey(0)
